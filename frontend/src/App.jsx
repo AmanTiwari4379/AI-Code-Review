b30dd1272f69c32d442be744af1ad478a81841fe
AmanTiwari4379 <taman4379@gmail.com>
2026-09-19 19:54:25 +0530

    Starting new project with frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [toggle, setToggle] = useState();
+  const toggle = '<h1>Hello World!</h1>';
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
